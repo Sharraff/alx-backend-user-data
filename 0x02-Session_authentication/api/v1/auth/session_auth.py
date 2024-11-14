8b2e9bf2fa8face4c674c9dccf0a18b1188a10e2
Sharraff <rogandsteinporter@gmail.com>
2024-11-14 04:54:02 -0800

    shit, another test

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -21,7 +21,7 @@ class SessionAuth(Auth):
         if user_id is None or not isinstance(user_id, str):
             return None
         session_id = str(uuid4())
-        SessionAuth.user_id_by_session_id[user_id] = user_id
+        SessionAuth.user_id_by_session_id[session_id] = user_id
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
